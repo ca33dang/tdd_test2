2ace4e104bfad1c97daf96435807e48d7a385763
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-06-27 10:12:49 -0700

    get the test to pass w/out error

--- test2.rb
+++ test2.rb
@@ -1,14 +1,45 @@
-def check_for_match(myticket, winnigticket)
-	my_ticket = myticket.split("")
+def number_comparison(my_ticket, winning_ticket)
+	my_ticket = my_ticket.split('')
 	puts "my ticket is #{my_ticket}"
-	winning_ticket = winningticket.split("")
-	puts "winningticket is #{winnig_ticket}"
-	matching_num = 0
-	winningticket.each_with_index do |num,index|
-		puts "num is #{num} and index is #{index}"
-		if num == myticket[index]
-			matching_num += 1
+	winning_ticket = winning_ticket.split('')
+	puts "winning ticket is #{winning_ticket}"
+	matching_number = 0
+	winning_ticket.each_with_index do |num,index|
+	puts "number is #{num} and index is #{index}"
+		if num == my_ticket[index]
+			matching_number = matching_number + 1
 		end
     end
-	matching_num
+	matching_num = matching_number + 1
 end
+	def check_for_match_arr(myticketarray, winningticket)
+		empty = []
+		myticketarray.each do |num|
+			number_comparison(num, winningticket)
+			if number_comparison(num, winningticket) == 1
+				empty << num
+			end
+		    end
+		    empty
+	end
+	def check_for_match_arr_2(myticketarray, winningticket)
+		empty = []
+		myticketarray.each do |num|
+			empty << number_comparison(num, winningticket)
+		end
+		empty
+	end
+	def winarr(myticketarray, winningticket)
+		warr = []
+		winningticket.each do |num|
+			matches = check_for_match_arr_2(myticketarray, num)
+			winners = 0
+			matches.each do |m|
+				if m == 0
+					winners += 1
+				end
+			end
+			warr << winners
+		end
+		warr
+	end
--- test_test2.rb
+++ test_test2.rb
@@ -3,19 +3,20 @@ require_relative "test2.rb"
 
 class TestTest < Minitest::Test
 
-	def test_assert_that_1_equals_1
-		assert_equal(1, 1)
+	def test_number_comparison_returns_number
+		assert_equal(Integer, number_comparison("", "").class)
+	end
+	def test_that_1_match_returns_3
+		assert_equal(3, number_comparison("1111", "1222"))
+	end
+	def test_that_2_match_returns_2
+		assert_equal(2, number_comparison("1111", "1122"))
+	end
+	def test_if_one_offs_return
+		assert_equal(["1235"], check_for_match_arr(["1222","1235","1234"], "1234"))
+	end
+	def test_if_winners_equal_2
+		assert_equal([2, 0], winarr(["1234", "1234", "1244", "1255"], ["1234", "1299"]))
 	end
 
-	def test_assert_comparison_returns_number
-		assert_equal(Integer, check_for_match("","").class)
-	end
-
-	def test_that_1_match_returns_1
-		assert_equal(1, check_for_match("1111","1222"))
-	end
-
-	def test_that_2_matches_returns_2
-		assert_equal(2, check_for_match("1111","1122"))
-	end
 end
